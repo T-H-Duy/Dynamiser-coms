1ffce72f654373c2d0a97b642608a4e516db1e85
Tang77 <nhduy287@gmail.com>
2024-04-20 17:59:31 +0200

    v2.10-addComment

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-let form = document.querySelector('form');
-let prenomForm = document.getElementById('first-name');
-let nomForm = document.getElementById('last-name');
-let messageForm = document.getElementById('message');
-let messageErreur = document.getElementById('error-message');
+const form = document.querySelector('form');
+const prenomForm = document.getElementById('first-name');
+const nomForm = document.getElementById('last-name');
+const messageForm = document.getElementById('message');
+const messageErreur = document.getElementById('error-message');
+const listeCommentaires = document.getElementById('comment-list');
+
+function newDiv(comment) {
+  let newCom = document.createElement('div');
+    newCom.innerHTML = `<div class="flex-1">
+        <h3 class="font-medium text-gray-900">${comment.prenomForm} ${comment.nomForm}</h3>
+        <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+          <p>${comment.messageForm}</p>
+        </div>
+      </div>`;
+      
+return newCom;
+}
 
 function envoiCom(event) {
+  let newCom = {prenomForm: prenomForm.value, nomForm: nomForm.value, messageForm: messageForm.value};
+  let newComItem = newDiv(newCom); 
 
   if (!prenomForm.value || !nomForm.value || !messageForm.value) {
   messageErreur.style.display = 'block';
@@ -12,12 +27,8 @@ function envoiCom(event) {
   messageErreur.style.display = 'none';
   }
 
+listeCommentaires.appendChild(newComItem);
 event.preventDefault();
-
-let newCom = {
-  prenomForm: prenomForm.value, nomForm: nomForm.value, messageForm: messageForm.value
-};
-
 form.reset();
 }
 
